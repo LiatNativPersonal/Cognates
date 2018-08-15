5abbda27e6bd4d26870654510190fa69db57664b
LiatNativ <liatnativ@gmail.com>
2018-08-15 22:26:34 +0300

    Adding main

---
+++ Cognates.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 22:16:43 2018
+
+@author: TAL-LAPTOP
+"""
+import os
+
+NATIVE_INPUT_DIR = "C:/Users/TAL-LAPTOP/Desktop/NLP Lab/rawData/reddit.Native/"
+NON_NATIVE_INPUT_DIR = "C:/Users/TAL-LAPTOP/Desktop/NLP Lab/rawData/reddit.nonNative/"
+
+def collectUserStatistics():
+    
+    return
+
+def collectStatistics():
+  
+    return
+
+
+if __name__ == '__main__':
+    print("Entering function __main__")
+    
+    print("Leaving function __main__")
