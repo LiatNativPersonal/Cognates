9fc6012ca36a12e811d0c7705648f8c328790a35
LiatNativ <liatnativ@gmail.com>
2018-08-20 10:23:42 +0300

    first working version

--- Cognates.py
+++ Cognates.py
@@ -9,9 +9,13 @@ import re
 
 NATIVE_INPUT_DIR = "C:/Users/TAL-LAPTOP/Desktop/NLP Lab/rawData/reddit.Native/"
 NON_NATIVE_INPUT_DIR = "C:/Users/TAL-LAPTOP/Desktop/NLP Lab/rawData/reddit.nonNative/"
+OUTPUT_FILE = "UserAmountOfContent.csv"
 
-def collectUserStatistics():
-    
+def printStatistics(stat_dict):    
+    for country,users_dict in stat_dict.items():
+        with open(country+OUTPUT_FILE,"w+", encoding="utf-8") as output:            
+            for user,counts in users_dict.items():            
+                output.write("'" + user + "', " + str(counts[0]) + " ," + str(counts[1]) + "\n")           
     return
 
 def clean_line(line):
@@ -27,6 +31,8 @@ def remove_subreeddit_and_user_name_from_line(line):
     l_line = l_line[l_line.find(']')+1:]
     return l_line
 
+#Construct a dictionary that maps each country to its users, 
+#and for each user holds the amount of tokens and sentences
 def collectStatistics():
     countries_to_users_dict = {}
     for file in (os.listdir(NATIVE_INPUT_DIR)):
@@ -37,23 +43,24 @@ def collectStatistics():
         countries_to_users_dict[country_name] = {}
         with open(NATIVE_INPUT_DIR + file, 'r', encoding='utf-8', errors='ignore') as country_file:
             for line in country_file:
-                user_name = get_user_name_from_line(line)
+                # user_name apears in the begining of every row inside []
+                user_name = get_user_name_from_line(line)     
+                # remove meta-data and extra characters
                 line = remove_subreeddit_and_user_name_from_line(line)                
                 line = clean_line(line)
+                
+                number_of_tokens = len(line.split())
                 if user_name in countries_to_users_dict[country_name].keys():
-                    countries_to_users_dict[country_name][user_name] += len(line.split())
-                    if user_name == 'zedok12':
-                        print (str(line.split()) + ' ' + str(len(line.split())) +  '\n' )
+                    countries_to_users_dict[country_name][user_name][0] += 1
+                    countries_to_users_dict[country_name][user_name][1] += number_of_tokens                   
                 else:
-                    countries_to_users_dict[country_name][user_name] = len(line.split())
-#                    if user_name == 'zedok12':
-#                        print (str(line.split()) + '\n')
-#        country = file.substring(len("reddit."), file.find()
-        print(countries_to_users_dict)
-    return
+                    count_user_text = [1, number_of_tokens]
+                    countries_to_users_dict[country_name][user_name] = count_user_text
+    return countries_to_users_dict
 
 
 if __name__ == '__main__':
     print("Entering function __main__")
-    collectStatistics()
+    stat_dict = collectStatistics()
+    printStatistics(stat_dict)
     print("Leaving function __main__")
